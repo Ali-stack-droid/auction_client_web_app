1b14baa8fd6ae79929f84ed9e95c348d86d2b677
HamidAli407 <hamidali407@gmail.com>
2025-01-16 00:55:31 +0500

    Footer Done

--- src/components/layout/AppProvider.tsx
+++ src/components/layout/AppProvider.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { Box, Divider } from '@mui/material';
+import { Box, Divider, IconButton, Typography } from '@mui/material';
 import Header from './header/Header'; // Import your Header component
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const AppProvider = ({ children }: any) => {
     const [searchTerm, setSearchTerm] = useState("");
@@ -30,6 +33,79 @@ const AppProvider = ({ children }: any) => {
                 {/* Content area below the header */}
                 <Box id="childContainer" sx={{ flex: 1, overflowY: 'auto', padding: "0 80px", }}>
                     {React.cloneElement(children, { searchTerm })}
+                </Box>
+
+                {/* Footer on bottom */}
+
+                <Box
+                    sx={{
+                        backgroundColor: '#012868',
+                        padding: '95px 120px 25px 120px',
+                    }}
+                >
+                    <Box sx={{
+                        color: '#ffffff', display: 'flex',
+                        flexDirection: { xs: 'column', sm: 'row' },
+                        justifyContent: 'flex-start',
+                        alignItems: { xs: 'flex-start', sm: 'flex-start' },
+                        gap: { xs: 8, sm: 4, md: 20 },
+                    }}>
+                        {/* Left Section: Logo and Description */}
+                        <Box sx={{ maxWidth: '366px' }}>
+                            <Typography sx={{ fontWeight: '600', fontSize: '24px', marginBottom: '18px', color: '#ffffff' }}>
+                                Logo
+                            </Typography>
+                            <Typography mb={'16px'} variant="body2" sx={{ lineHeight: 1.8, fontSize: '16px', fontWeight: 400 }}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus feugiat lectus
+                                risus sed ullamcorper. Auctor semper fermentum
+                            </Typography>
+                            <Typography variant="body2" sx={{ lineHeight: 1.8, fontSize: '16px', fontWeight: 400 }}>
+                                volutpat integer vel. In rhoncus
+                                elementum nunc, malesuada mi sed. Nibh est sit lobortis id semper.
+                            </Typography>
+                        </Box>
+
+                        {/* Middle Section: Useful Links */}
+                        <Box>
+                            <Typography variant="h6" sx={{ fontWeight: '600', fontSize: '24px', marginBottom: '18px', color: '#ffffff' }}>
+                                Useful Links
+                            </Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '6px' }}>
+                                <a href="#" style={{ color: '#ffffff', textDecoration: 'none' }}>
+                                    Auctions
+                                </a>
+                                <a href="#" style={{ color: '#ffffff', textDecoration: 'none' }}>
+                                    View Catalog
+                                </a>
+                                <a href="#" style={{ color: '#ffffff', textDecoration: 'none' }}>
+                                    Testimonial
+                                </a>
+                                <a href="#" style={{ color: '#ffffff', textDecoration: 'none' }}>
+                                    Features
+                                </a>
+                            </Box>
+                        </Box>
+                    </Box>
+
+                    {/* Right Section: Social Icons */}
+                    <Box display={'flex'} justifyContent={'flex-end'}>
+                        <Box sx={{ display: 'flex', gap: '12px', marginBottom: '16px' }}>
+                            <IconButton sx={{ color: '#ffffff' }}>
+                                <FacebookIcon />
+                            </IconButton>
+                            <IconButton sx={{ color: '#ffffff' }}>
+                                <InstagramIcon />
+                            </IconButton>
+                            <IconButton sx={{ color: '#ffffff' }}>
+                                <TwitterIcon />
+                            </IconButton>
+                        </Box>
+
+                    </Box>
+                    <Divider sx={{ backgroundColor: '#ffffff' }} />
+                    <Typography mt={3} variant="body2" sx={{ fontSize: '16px', fontWeight: 400, textAlign: 'center', color: '#ffffff' }}>
+                        © 2022 All Right Reserved
+                    </Typography>
                 </Box>
             </Box>
         </Box>
